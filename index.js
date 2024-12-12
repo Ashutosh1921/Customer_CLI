7c8b30221bb34d3b851cf1b9896fbaae14d78515
Ashutosh1921 <as7857139@gmail.com>
2024-12-12 22:53:12 +0530

    2nd stage done and running

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const { Command } = require("commander");
 const { addCustomer, findCustomers, updateCustomer, deleteCustomer, AllCustomer } = require("./customer_cli/app");
 const program = new Command();
